544e8cb3debdada4490aa591743da1be80fc4c6a
Bryant-Wang24 <485969746@qq.com>
2021-02-04 17:12:56 +0800

    创建 <Layout />,我与重复不共戴天

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      statistics
-    </div>
-    <Nav/>
-  </div>
+  <Layout>
+    statistics
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -14,16 +11,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
 
-.content {
-  border: 1px solid blue;
-  overflow: auto;
-  flex-grow: 1;
-}
 </style>
