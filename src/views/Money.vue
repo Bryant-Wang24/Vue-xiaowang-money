5397982791e18b371ceaea5366e47f5fd6944cf6
Bryant-Wang24 <485969746@qq.com>
2021-02-05 22:58:22 +0800

    收集四个组件的数据到 record

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,27 +1,45 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad/>
-    <Notes/>
-    <Tags :data-source.sync="tags"/>
-    <Types/>
+    {{ record }}
+    <NumberPad :value.sync="record.amount"/>
+    <Notes @update:value="onUpdateNotes"/>
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+    <Types :value.sync="record.type"/>
   </Layout>
 </template>
 
 <script lang="ts">
+import Vue from 'vue';
 import Notes from '@/components/Money/Notes.vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Tags from '@/components/Money/Tags.vue';
 import Types from '@/components/Money/Types.vue';
+import {Component} from 'vue-property-decorator';
 
-export default {
-  name: 'Money',
-  components: {Types, Tags, NumberPad, Notes},
-  data() {
-    return {
-      tags: ['服饰', '餐饮', '住房', '交通', '医疗', '购物', '娱乐', '教育']
-    };
+type Record = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
+}
+@Component({
+  components: {Tags, Notes, Types, NumberPad}
+})
+
+export default class Money extends Vue {
+  tags = ['服饰', '餐饮', '住房', '交通', '医疗', '购物', '娱乐', '教育'];
+  record: Record = {
+    tags: [], notes: '', type: '-', amount: 0
+  };
+
+  onUpdateTags(value: string[]) {
+    this.record.tags = value;
   }
-};
+
+  onUpdateNotes(value: string) {
+    this.record.notes = value;
+  }
+}
 </script>
 
 <style lang="scss">
