7aa04d75abab90ab5974babca7299c2d30d75bf2
Bryant-Wang24 <485969746@qq.com>
2021-02-05 11:05:20 +0800

    HTML 和 CSS 完成

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,7 +22,6 @@ export default {
 @import "~@/assets/style/helper.scss";
 nav {
   display: flex;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   flex-direction: row;
   font-size: 12px;
   > .item {
