fd3c3fee09760a652779926935e3d29435e6fa8e
Bryant-Wang24 <485969746@qq.com>
2021-02-08 19:25:39 +0800

    封装 tagList 的增删改查

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -15,11 +15,9 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import tagListModel from '@/models/tagListModel';
 import {Component} from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
 
-tagListModel.fetch();
 @Component({
   components: {Button}
 })
