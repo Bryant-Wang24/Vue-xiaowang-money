2f683829f9db8b17684eb57041bc72848c9632d1
Bryant-Wang24 <485969746@qq.com>
2021-02-09 23:33:45 +0800

    删除多余的代码

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -4,7 +4,7 @@ import Component from 'vue-class-component';
 
 @Component
 export class TagHelper extends Vue {
-  create() {
+  createTag() {
     const name = window.prompt('请输入标签名');
     if (!name) {return window.alert('标签名不能为空');}
     this.$store.commit('createTag',name)
