72ff9a9219fb6f6c67c2345b2d67b5ef2d713884
Bryant-Wang24 <485969746@qq.com>
2021-02-09 13:57:45 +0800

    Money.vue 使用 Vuex

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -6,8 +6,6 @@
     </div>
     <Tags/>
     <Types :value.sync="record.type"/>
-    {{count}}
-    <button @click="add">+1</button>
   </Layout>
 </template>
 
@@ -18,37 +16,34 @@ import Tags from '@/components/Money/Tags.vue';
 import Types from '@/components/Money/Types.vue';
 import {Component} from 'vue-property-decorator';
 import FormItem from '@/components/Money/FormItem.vue';
-import store from '@/store/index2';
 
 
 @Component({
   components: {Tags, FormItem, Types, NumberPad},
   computed: {
-    count() {
-      return store.count;
-    },
     recordList() {
-      return store.recordList;
-    }
+      return this.$store.state.recordList;
+    },
   }
 })
 
 export default class Money extends Vue {
   //['服饰', '餐饮', '住房', '交通', '医疗', '购物', '娱乐', '教育'];
-  add() {
-    store.addCount();
-  }
 
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
+
+  created(){
+    this.$store.commit('fetchRecords')
+  }
 
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
 
   saveRecord() {
-    store.createRecord(this.record);
+    this.$store.commit('createRecord',this.record)
   }
 }
 </script>
